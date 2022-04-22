bbafbbaa38a623020cc3d7df9d6293309aab99cc
Hyewon Joo <hyewonjoo@Hyewons-MBP.lan>
2022-04-21 21:22:07 -0700

    for today

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import icon from './styles/birthday-cake.png';
 
 const App = () => {
+
+    const [size, setSize] = useState(500);
+
+    const handleClick = () => {
+        setSize(size === 500 ? 200 : 500);
+    }
+
     return (
-        <div>
-            <img src={icon} alt="icon" />
+        <div style={divstyle}>
+            <img src={icon} width={`${size}px`} alt="icon" onClick={handleClick} />
         </div>
     );
 }
+
+const divstyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
 export default App;
